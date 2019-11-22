831251125b4f9e1f7203fabefbea158d8d7665a1
Sven Schrodt <sven@schrodt-service.net>
2019-11-22 17:21:47 +0100

    Adding resources for Unit testing

---
+++ test/SvenSchrodt/Rechnungswesen/Doppik/Konto/Ikr/Model.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * SvenSchrodt\Rechnungswesen\Doppik\Konto\Ikr\Model
+ * 
+ * 
+ * Model chart of accounts of German IKR ("Industriekontenrahmen")
+ * 
+ * 
+ * Hint: due to the fact, that this project is addressing German accounting schema,
+ * the naming convention of classes, methods and variables/attributes etc. will use
+ * German terms and abbreviations whenever it seems to be useful
+ *
+ * @link https://github.com/svenschrodt/Rechnungswesen
+ * @author Sven Schrodt<sven@schrodt-service.net>
+ * @package SvenSchrodt\Rechnungswesen
+ * @license https://github.com/svenschrodt/Rechnungswesen/blob/master/LICENSE.md
+ * @copyright Sven Schrodt<sven@schrodt-service.net>
+ * @version 0.0.1
+ * @since 2019-11-21
+ */
+namespace SvenSchrodt\Rechnungswesen\Doppik\Konto\Ikr;
+
+use PHPUnit\Framework\TestCase;
+
+class ModelTest extends TestCase
+{
+
+ public function testAccountName()
+ {
+     
+ }
+}
+
